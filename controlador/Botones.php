878850c879f21089b84938938ef6a0e478d3444f
edu674 <77554668+edu674@users.noreply.github.com>
2022-01-31 14:49:21 -0600

    edu674

--- controlador/Botones.php
+++ controlador/Botones.php
@@ -26,10 +26,13 @@ switch ($opcion) {//switch de aumentar o disminuir cantidad
          for ($i=0; $i <count($arreglo) ; $i++) { //a traves del for se busca el elemento que quiere sera modificado
 	         if ($arreglo[$i]['id']==$id) { //el if compara que el id que busquemos se encuentree dentro del arreglo
 		         $arreglo[$i]['cantidad']=$arreglo[$i]['cantidad']-1;//una vez que lo encuentra le resta uno
-		         if ($arreglo[$i]['cantidad']==0) {
+		         if ($arreglo[$i]['cantidad']<=0) {//en caso de que la cantidad del producto sea igual o menor a 0 el producto se borrara
+		         	
+                    // redireccionamos al controlador eliminar carrito con el objetivo de eliminar y le pasamos atraves de get el id del productoque vamos a eliminar
 		         	echo'<script> 
-                    window.location.href="/eliminarCarrito.php?id='.$id.'
-                    </script>';	        
+                    window.location.href="../controlador/eliminarCarrito.php?id='.$arreglo[$i]['id'].'";
+                    </script>';
+	        
 		         }
 		         break;//rompemos el ciclo para que no continue una vez que ya encontro el dato que queremos modificar
 	         }
